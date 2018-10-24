199685481b6435b7ea5c08f25c2f56991d3c850f
hasin <hasin_ahbab@yahoo.com>
2018-10-24 16:59:29 +0600

    First Commit

---
+++ CsharpGenerics/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System.Web.Script.Serialization;
+
+
+namespace CsharpGenerics
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            string jsonDataFromURL = DataFromAPI<int>.getFromUrl(3);
+
+            Console.WriteLine(jsonDataFromURL);
+            Console.Read();
+        }
+    }
+
+    public class DataFromAPI<T>
+    {
+        public static string getFromUrl(T userId)
+        {
+
+            using (WebClient wc = new WebClient())
+            {
+                string url = wc.DownloadString($"https://reqres.in/api/users/{userId}");
+                JavaScriptSerializer javaScriptSerializer = new JavaScriptSerializer();
+                RootObject rootObject = (RootObject)javaScriptSerializer
+                    .Deserialize(url, typeof(RootObject));
+
+
+                return "Id: " + rootObject.data.id + "\nFirst Name: "
+                    + rootObject.data.first_name + "\nLast Name: "
+                    + rootObject.data.last_name;
+
+            }   
+        }
+    }
+}
---
+++ CsharpGenerics/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsharpGenerics
+{
+    public class RootObject
+    {
+        public data data { get; set; }
+    }
+
+    public class data
+    {
+        public int id { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string avatar { get; set; }
+    }
+}
